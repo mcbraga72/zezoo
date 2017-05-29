02bd25eea7c92dfbddd1c4d4d01a35c674fd5def
Marcelo Braga <mcbraga@hotmail.com>
2017-05-28 22:11:31 -0300

    Models creation.

---
+++ protected/models/Customer.php
@@ -0,0 +1,37 @@
+<?php
+
+Class Customer extends CActiveRecord 
+{
+
+	public function tableName()
+	{
+		return 'customers';
+	}
+
+	public function relations() {
+        return array(
+            'contacts' => array(self::HAS_MANY, 'Contact', 'customer_id'),
+        );
+    }
+
+	public function rules()
+	{
+		return [
+			[['name', 'email'], required],
+			['email', 'email']			
+		]
+	}
+
+	public function attributeLabels()
+	{
+		return [
+			'name' => 'Nome',
+			'email' => 'E-mail'			
+		]
+	}
+
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
